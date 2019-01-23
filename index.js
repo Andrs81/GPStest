a4ffe2c2de7910009127f5e0430004567983f7e0
Henry <cyberdelahoz95@gmail.com>
2019-01-23 12:44:06 -0500

    index function

---
+++ index.js
@@ -0,0 +1,36 @@
+var gpsd = require('node-gpsd');
+
+var currentLatitude = 0.0;
+var currentLongitude = 0.0;
+
+function PositionBasedOnGPSD (program, device, port) {
+
+    var daemon = new gpsd.Daemon({
+        port: port,
+        program: program,
+        device: device,
+    });
+
+    daemon.start(function() {
+        var listener = new gpsd.Listener({port: port});
+    
+        listener.on('TPV', function (tpv) {
+	    console.log(tpv);
+
+	    if (tpv.mode === 2 || tpv.mode === 3) {
+	        currentLatitude = tpv.lat;
+	        currentLongitude = tpv.lon;
+	    }
+        });
+    
+        listener.connect(function() {
+            listener.watch();
+        });
+    });
+}
+
+PositionBasedOnGPSD.prototype.getPosition = function() {
+    return {latitude: currentLatitude, longitude: currentLongitude};
+}
+
+module.exports = PositionBasedOnGPSD;
