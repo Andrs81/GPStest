69426410c28a5411cd43a55a2cd2cb60a238ff30
Henry <cyberdelahoz95@gmail.com>
2019-01-25 12:48:57 -0500

    pruebas

--- index.js
+++ index.js
@@ -21,12 +21,11 @@ function PositionBasedOnGPSD (program, device, port) {
         var listener = new gpsd.Listener({port: port});
     
         listener.on('TPV', function (tpv) {
-            console.log(tpv);
-            console.log(tpv.mode);
 
             if (tpv.mode === 2 || tpv.mode === 3) {
                 currentLatitude = tpv.lat;
                 currentLongitude = tpv.lon;
+                console.log(currentLatitude, currentLongitude);
             }
         });
     
